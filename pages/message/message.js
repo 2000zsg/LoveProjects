c8dbd5d03c5d1462ca9a7ef85f2fd48e0fe1a54e
张大帅 <70946742+2000zsg@users.noreply.github.com>
2021-08-08 00:10:03 +0800

    爱心商城

---
+++ pages/message/message.js
@@ -0,0 +1,40 @@
+var app = getApp()
+var ln = app.ln2
+
+Page({
+
+  data: {
+
+  },
+
+  onLoad: function (options) {
+
+  },
+
+  onReady: function () {
+
+  },
+
+  onShow: function () {
+
+  },
+
+  onHide: function () {
+
+  },
+  onUnload: function () {
+
+  },
+
+  onPullDownRefresh: function () {
+
+  },
+
+  onReachBottom: function () {
+
+  },
+
+  onShareAppMessage: function () {
+
+  }
+})
---
+++ pages/shopList6/shopList6.js
@@ -0,0 +1,77 @@
+var app = getApp()
+var ln = app.ln2
+Page({
+
+  /**
+   * Page initial data
+   */
+  data: {
+    IP: app.IP2,
+    ln: app.ln2,
+    food: '',
+    weddings: '',
+    KTV: '',
+    job: '',
+    lesson: '',
+    renovation: '',
+    tuneUp: '',
+    rental: '',
+    more: '' 
+  },
+
+  /**
+   * Lifecycle function--Called when page load
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page is initially rendered
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page show
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page hide
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page unload
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * Page event handler function--Called when user drop down
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * Called when page reach bottom
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * Called when user click on the top right corner to share
+   */
+  onShareAppMessage: function () {
+
+  }
+})
